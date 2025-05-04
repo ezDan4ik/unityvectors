fc213a4cb717e575743ce81083ca6ac35f6f3363
ezDan4ik <danka0804@gmail.com>
2025-05-04 13:07:29 +0300

    first commit

---
+++ Assets/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 5f;
+
+    void Start() {
+
+
+       //this.transform.Translate(goal.transform.position);
+       // this.transform.Translate(6, 0, 2);
+       //direction = goal.transform.position - this.transform.position;
+       
+        //this.transform.Translate{direction};
+    }
+
+    private void LateUpdate() 
+    {
+        //this.transform.position = this.transform.position + direction * 0.01f;
+        //this.transform.position = this.transform.position + direction.normalized;
+        direction = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (direction.magnitude > 2) 
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+    }
+}
